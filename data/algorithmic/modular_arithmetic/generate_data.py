b8a8eabfc602d219cfb02ca11c3b40f8c11d9fce
Mateusz Pyla <mateuszpyla@gmail.com>
2025-09-19 00:57:52 +0200

    indicate train_fraction in subdir in artithmetic data path

--- data/algorithmic/modular_arithmetic/generate_data.py
+++ data/algorithmic/modular_arithmetic/generate_data.py
@@ -104,7 +104,7 @@ def save_dataset(
     """Save dataset to files with metadata"""
     output_path = Path(output_dir)
     # create a subdirectory named by prime and operation (e.g. "./modular_arithmetic_data/prime_97_addition")
-    subdir = f"prime_{config['prime']}_{config['operation']}"
+    subdir = f"prime_{config['prime']}_{config['operation']}_trainfrac_{config['train_fraction']}"
     output_path = output_path / subdir
     output_path.mkdir(parents=True, exist_ok=True)
     
